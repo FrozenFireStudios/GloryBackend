984a47e8d046c6fff20afd18597c923d62c25945
Marcus <marcus@frozenfirestudios.com>
2017-03-19 23:16:30 -0400

    Add logs

--- lib/controller/match_controller.dart
+++ lib/controller/match_controller.dart
@@ -1,3 +1,4 @@
+import 'package:http/http.dart' as http;
 import '../glory_backend.dart';
 import 'dart:io';
 import 'dart:convert';
@@ -21,53 +22,39 @@ class MatchController extends HTTPController {
     int offset = ((page ?? 1) - 1) * 50;
     createdAt ??= "2017-03-01T00:00:00Z";
 
-    var urlString = Uri.encodeFull("https://api.dc01.gamelockerapp.com/shards/${shard}/matches?filter[playerNames]=${playerName}&filter[createdAt-start]=${createdAt}&filter[gameMode]=ranked,casual,private&page[limit]=50&page[offset]=${offset}");
+    String urlString = Uri.encodeFull("https://api.dc01.gamelockerapp.com/shards/${shard}/matches?filter[playerNames]=${playerName}&filter[createdAt-start]=${createdAt}&filter[gameMode]=ranked,casual,private&page[limit]=50&page[offset]=${offset}");
+    String apiKey = Platform.environment["VG_API_KEY"];
+    Map<String, String> headers = {
+      "Authorization": "Bearer ${apiKey}",
+      "X-TITLE-ID": "semc-vainglory",
+      "Accept": "application/vnd.api+json",
+      "Accept-Encoding": "gzip"
+    };
 
-    HttpClient client = new HttpClient();
-    client.getUrl(Uri.parse(urlString))
-      .then((HttpClientRequest request) {
-        String apiKey = Platform.environment["VG_API_KEY"];
-        request.headers.set("Authorization", "Bearer ${apiKey}");
-        request.headers.set("X-TITLE-ID", "semc-vainglory");
-        request.headers.set("Accept", "application/vnd.api+json");
-        request.headers.set("Accept-Encoding", "gzip");
+    http.get(urlString, headers: headers)
+    .then((response) {
+      if (response.statusCode == 200) {
+        String json = response.body;
+        List<VGMatch> matches = VGMatch.matchesFrom(json, items);
+        List matchMaps = matches.map((m) {
+          List pMaps = m.participants.map((p) => p.asMap()).toList();
+          return {
+            "id": m.id,
+            "duration": m.duration,
+            "participants": pMaps
+          };
+        }).toList();
 
-        return request.close();
-      })
-      .then((HttpClientResponse response) {
-        if (response.statusCode == 200) {
-          response.transform(UTF8.decoder).listen((String data) {
-            contents.write(data);
-          }, onDone: () {
-            String json = contents.toString();
-            List<VGMatch> matches = VGMatch.matchesFrom(json, items);
-            List matchMaps = matches.map((m) {
-              List pMaps = m.participants.map((p) => p.asMap()).toList();
-              return {
-                "id": m.id,
-                "duration": m.duration,
-                "participants": pMaps
-              };
-            }).toList();
-
-            completer.complete(new Response.ok(matchMaps));
-          });
-        } else {
-          print("\nStatus Code: ${response.statusCode}\n");
-          print("URL: ${urlString}\n");
-          completer.complete(new Response.serverError());
-        }
-      });
+        print("MARCUS:\n${matchMaps}\n");
+        
+        completer.complete(new Response.ok("Got ${matchMaps.length} matches"));
+      } else {
+        print("\nStatus Code: ${response.statusCode}\n");
+        print("URL: ${urlString}\n");
+        completer.complete(new Response.serverError());
+      }
+    });
 
     return completer.future;
-  }
-
-  List<VGMatch> matchesFrom(String json) {
-
-
-
-
-
-
   }
 }
